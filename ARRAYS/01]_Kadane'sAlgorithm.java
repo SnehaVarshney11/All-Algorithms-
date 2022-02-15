95b50fd2c06982b060b9d7682044629193b3c81f
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-02-15 20:07:15 +0530

    Kadane's Algorithm

---
+++ ARRAYS/01]_Kadane'sAlgorithm.java
@@ -0,0 +1,18 @@
+// Used to find the contiguous sub-array(containing at least one number) which has the maximum sum.
+
+package ARRAYS;
+
+class KadaneAlgorithm {
+    long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        long max_so_far = arr[0];
+        long curr_max = arr[0];
+        
+        for(int i = 1; i < n; i++){
+            curr_max = Math.max(arr[i] , curr_max + arr[i]);
+            max_so_far = Math.max(curr_max, max_so_far);
+        }
+        return max_so_far;
+    }
+}
